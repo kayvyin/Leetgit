43198d0618a3aefc0834ba44440005d9e23f95a1
kayvyin <calvinhuynhh@gmail.com>
2022-07-13 12:06:30 -0700

    Time: 5 ms (23.37%), Space: 6.4 MB (15.90%) - LeetHub

--- 1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
@@ -1,31 +1,50 @@
 class Solution {
 public:
-    int maxRepeating(string sequence, string word) {
-        if(sequence == word) return true;
-        if(word.length() > sequence.length()) return false;
-
-        int wlen = word.length();
-        int slen = sequence.length();
-        int ans = 0;
-        for(int i = 0; i < slen - wlen + 1; i++){
-            int j = i;
-            int k = 0;
-            int curr = 0;
-            while(j < slen){
-                if(word[k] == sequence[j]){
-                    j++;
-                    k++;
-                }
-                else{
-                    break;
-                }
-                if(k == wlen){
-                    curr++;
-                    k = 0;
-                }
+    int maxRepeating(string s, string w) {
+        
+        // store original word length
+        int wl = w.length();
+        int sl = s.length();
+        
+        // build up word and preprocess LPS
+        while(w.size() < sl)w+=w;
+        
+        vector<int>lps(w.length());
+        lps[0] = 0;
+        
+        int i = 1;
+        int j = 0;
+        while(i < w.length())
+        {
+            if(w[i]==w[j])lps[i++]=++j;
+            else
+            {
+                if(j==0)lps[i++]=0;
+                else j = lps[j-1];
             }
-            ans = max(ans, curr);
         }
-        return ans;
+        
+        // run KMP over updated word and sequence string
+        i = 0;
+        j = 0;
+        int maxc = 0;
+        while(i < sl)
+        {
+            if(s[i]==w[j])i++,++j;
+            else
+            {
+                if(j==0)i++;
+                else j = lps[j-1];
+            }
+            
+
+            // word pointer divisible by original word length and so is a repeated substring
+            // repeat count => j / word length and track the max repeat
+            if((j%wl)==0)
+                maxc = max(maxc,j/wl);
+        }
+        
+        // return max repeat count
+        return maxc;        
     }
 };
