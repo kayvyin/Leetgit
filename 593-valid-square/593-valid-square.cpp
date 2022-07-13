2a284521f1e9916e115b619d9b88c7b3c27eecea
kayvyin <calvinhuynhh@gmail.com>
2022-07-13 13:58:26 -0700

    Time: 11 ms (62.55%), Space: 26.5 MB (96.54%) - LeetHub

--- 593-valid-square/593-valid-square.cpp
+++ 593-valid-square/593-valid-square.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
+    int sd(vector<int>& p1, vector<int>& p2) {
+        return pow(p1[0] - p2[0], 2) + pow(p1[1] - p2[1], 2);
+    }
     bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
-        vector<vector<int>> p = {p1, p2, p3, p4};
-        
-        unordered_set<int> lengths;
-        
-        for(int i = 0; i < 4; i++){
-            for(int j = i + 1; j < 4; j++){
-                int x_dist = abs(p[i][0] - p[j][0]);
-                int y_dist = abs(p[i][1] - p[j][1]);
-                
-                int dist = x_dist *  x_dist + y_dist * y_dist;
-                
-                if(dist > 0){
-                    lengths.insert(dist);
-                }
-                else{
-                    return false;
-                }
-            }
-        }
-        return lengths.size() == 2;
-        
-        
+        // support variables
+        int sd0, sd1, sd2, sd3;
+        vector<vector<int>> pts = {p1, p2, p3, p4};
+        sort(begin(pts), end(pts));
+        // computing the distances between ordered points
+        sd0 = sd(pts[0], pts[1]), sd1 = sd(pts[0], pts[2]), sd2 = sd(pts[0], pts[3]), sd3 = sd(pts[1], pts[2]);
+        // checking if sides are non-0 and if sides and diagonals are equal
+        return sd0 && sd2 && sd0 == sd1 && sd2 == sd3;
     }
 };
