c6749caa83960c5860648654fb076742b36ac157
kayvyin <calvinhuynhh@gmail.com>
2022-07-13 09:54:55 -0700

    Time: 22 ms (7.58%), Space: 13.7 MB (48.06%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -9,12 +9,14 @@ public:
         
         vector<int> dp(n);
         
-        dp[0] = cost[0];
-        dp[1] = cost[1];
+        int down1 = 0;
+        int down2 = 0;
         
-        for(int i = 2; i < n; i++){
-            dp[i] = min(cost[i] + dp[i -2], cost[i] + dp[i - 1]);
+        for(int i = 2; i < n + 1; i++){
+            int temp = down1;
+            down1 = min(down1 + cost[i - 1], down2 + cost[i - 2]);
+            down2 = temp;
         }
-        return min(dp[n - 1], dp[n - 2]);
+        return down1;
     }
 };
