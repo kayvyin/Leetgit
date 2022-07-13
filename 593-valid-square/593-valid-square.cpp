5511b955fd24318cc403051e1084713695055f17
kayvyin <calvinhuynhh@gmail.com>
2022-07-13 16:02:26 -0700

    Time: 7 ms (86.70%), Space: 26.6 MB (66.17%) - LeetHub

--- 593-valid-square/593-valid-square.cpp
+++ 593-valid-square/593-valid-square.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
-    int sd(vector<int>& p1, vector<int>& p2) {
-        return pow(p1[0] - p2[0], 2) + pow(p1[1] - p2[1], 2);
-    }
     bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
-        // support variables
-        int sd0, sd1, sd2, sd3;
-        vector<vector<int>> pts = {p1, p2, p3, p4};
-        sort(begin(pts), end(pts));
-        // computing the distances between ordered points
-        sd0 = sd(pts[0], pts[1]), sd1 = sd(pts[0], pts[2]), sd2 = sd(pts[0], pts[3]), sd3 = sd(pts[1], pts[2]);
-        // checking if sides are non-0 and if sides and diagonals are equal
-        return sd0 && sd2 && sd0 == sd1 && sd2 == sd3;
+        vector<vector<int>> points{p1, p2, p3, p4};
+        
+        unordered_set<int> hash;
+        
+        for(int i = 0; i < 4; i++){
+            for(int j = i + 1; j < 4; j++){
+                int x_dist = points[i][0] - points[j][0];
+                int y_dist = points[i][1] - points[j][1];
+                int squared_dist = x_dist * x_dist + y_dist * y_dist;
+                if(squared_dist == 0){
+                    return false;
+                }
+                hash.insert(squared_dist);
+                if(hash.size() > 2){
+                    return false;
+                }
+            }
+        }
+        return hash.size() == 2;
     }
 };
