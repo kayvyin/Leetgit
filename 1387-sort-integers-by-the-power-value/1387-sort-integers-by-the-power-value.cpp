98c6c5038dc96851da807d4a15b989e039ed7541
kayvyin <calvinhuynhh@gmail.com>
2022-07-16 12:44:06 -0700

    Time: 997 ms (5.03%), Space: 302.1 MB (7.20%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -1,40 +1,15 @@
-
 class Solution {
+    vector<int>dp=vector<int>(1000000);
+    int util(int x) {
+        if(x<2) return dp[x];
+        return dp[x]?dp[x]:dp[x]=1+(x&1?util(3*x+1):util(x/2));
+    }
 public:
     int getKth(int lo, int hi, int k) {
-        map<int, int> freq;
-        
-        while(lo <= hi){
-            int sol = getPow(lo);
-            freq[lo] = sol;
-            lo++;
-        }
-        
-        vector<pair<int, int> > result;
-        map<int, int>::iterator it;
-        
-        for(auto it : freq){
-            result.push_back({it.first, it.second});
-        }
-        
-        sort(result.begin(), result.end(),
-            [](const pair<int, int> &a, const pair<int, int> &b){
-                if(a.second != b.second) return (a.second < b.second);
-                else return a.first < b.first;
-        });
-        
-        return result[k - 1].first;
-    }
-    
-    int getPow(int x){
-        int count = 0;
-        if(x == 1) return 1;
-        while(x != 1){
-            if(x % 2 == 0) x = x / 2;
-            else x = (3 * x) + 1;
-            count++;
-        }
-        
-        return count;
+        vector<pair<int,int>> t;
+        for(int i=lo; i<=hi; i++)
+            t.push_back({util(i),i});
+        sort(t.begin(),t.end());
+        return t[k-1].second;
     }
 };
