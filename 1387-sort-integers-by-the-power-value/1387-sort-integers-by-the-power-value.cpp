bab0051c76cd94f8788442783cdbc505c982d0fd
kayvyin <calvinhuynhh@gmail.com>
2022-07-16 12:42:50 -0700

    Time: 100 ms (54.22%), Space: 15.6 MB (42.68%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -1,31 +1,40 @@
 
 class Solution {
 public:
-    struct greater_oper{
-        bool operator()(pair<int, int> &p1, pair<int, int> &p2){
-            if(p1.first == p2.first) return p1.second < p2.second;
-            else return p1.first < p2.first;            
-        }
-    };
-    
-    #define pis pair<int, int>
-    
-    int getKth(int lo, int hi, int k) {   
-        priority_queue<pis, vector<pis>, greater_oper> max_heap;
+    int getKth(int lo, int hi, int k) {
+        map<int, int> freq;
         
-        for(int curr = lo; curr <= hi; curr++){
-            int value = curr, count = 0;
-            
-            while(value != 1){
-                if(value % 2 == 0) value /= 2;
-                else value = 3 * value + 1;
-                count++;
-            }
-            
-            max_heap.push({count, curr});
-            if(max_heap.size() > k) max_heap.pop();
+        while(lo <= hi){
+            int sol = getPow(lo);
+            freq[lo] = sol;
+            lo++;
         }
         
-        return max_heap.top().second;
+        vector<pair<int, int> > result;
+        map<int, int>::iterator it;
+        
+        for(auto it : freq){
+            result.push_back({it.first, it.second});
+        }
+        
+        sort(result.begin(), result.end(),
+            [](const pair<int, int> &a, const pair<int, int> &b){
+                if(a.second != b.second) return (a.second < b.second);
+                else return a.first < b.first;
+        });
+        
+        return result[k - 1].first;
+    }
+    
+    int getPow(int x){
+        int count = 0;
+        if(x == 1) return 1;
+        while(x != 1){
+            if(x % 2 == 0) x = x / 2;
+            else x = (3 * x) + 1;
+            count++;
+        }
+        
+        return count;
     }
 };
